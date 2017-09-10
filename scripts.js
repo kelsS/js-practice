590fae4741ca504c39e149e3217cb7b56fe8f75a
Kelsey Swanson <kswanson426@gmail.com>
2017-09-10 17:00:50 -0400

    added practice scripts

---
+++ scripts.js
@@ -0,0 +1,16 @@
+var name = 'Kelsey';
+console.log(name);
+
+var lastName = 'Swanson';
+console.log(lastName);
+
+var age = 24;
+console.log(age);
+
+/**
+ * Find out if a person has a full age or * not
+ * Uses boolean logic
+ */ 
+
+var fullAge = true;
+console.log(fullAge);
